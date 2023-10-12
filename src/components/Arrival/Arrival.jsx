10c30ff1409c68e3b0bfbdbcba9f05a0a0f18835
Kirill Lee <1x1heavy@gmail.com>
2023-10-12 21:39:54 +0300

    dfdhdgf

--- src/components/Arrival/Arrival.jsx
+++ src/components/Arrival/Arrival.jsx
@@ -1,28 +1,47 @@
 import styles from "./Arrival.module.scss";
 import SectionHeading from "../UI/Heading/SectionHeading";
-import ArrivalCard from "./ArrivalCard/";
+import ArrivalCard from "./ArrivalCard";
 
 const Arrival = () => {
+  const ArrivalCard = [
+    {
+      id: "0",
+      title: "Knitted Joggers",
+      imageUrl: "./src/assets/images/Arrival/card1.png",
+    },
+    {
+      id: "1",
+      title: "Full Sleeve",
+      imageUrl: "./src/assets/images/Arrival/card2.png",
+    },
+    {
+      id: "2",
+      title: "Active T-Shirts",
+      imageUrl: "./src/assets/images/Arrival/card3.png",
+    },
+    {
+      id: "3",
+      title: "Urban Shirts",
+      imageUrl: "./src/assets/images/Arrival/card4.png",
+    },
+  ];
   return (
     <div className={styles.newArrival}>
       <SectionHeading title="New Arrival" />
-      
+
       <div className={styles.allCard}>
+        {ArrivalCard.map((item) => {
+          return (
+            <ArrivalCard
+              key={item.id}
+              title={item.title}
+              image={item.imageUrl}
+            />
+          );
+        })}
         <div className={styles.card}>
           <img src="/src/assets/images/Arrival/card1.png" alt="" />
           <p>Knitted Joggers</p>
-        </div>
-        <div className={styles.card}>
-          <img src="/src/assets/images/Arrival/card2.png" alt="" />
-          <p>Full Sleeve</p>
-        </div>
-        <div className={styles.card}>
-          <img src="/src/assets/images/Arrival/card3.png" alt="" />
-          <p>Active T-Shirts</p>
-        </div>
-        <div className={styles.card}>
-          <img src="/src/assets/images/Arrival/card4.png" alt="" />
-          <p>Urban Shirts</p>
         </div>
       </div>
     </div>
