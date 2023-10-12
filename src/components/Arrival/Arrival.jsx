2a38b845494b2d2690276678860ee06d039161fb
Kirill Lee <1x1heavy@gmail.com>
2023-10-12 20:16:11 +0300

    5464

---
+++ src/components/Arrival/Arrival.jsx
@@ -0,0 +1,10 @@
+import styles from './Arrival.module.scss';
+
+const Arrival = () => {
+    return (
+        <div className={styles.newArrival}>
+            <img src="" alt="" />
+            <h2 className=''>New Arrival</h2>
+        </div>
+    )
+}
